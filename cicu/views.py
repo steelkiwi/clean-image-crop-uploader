f37698ebc7af525a1bb18e413f6c11f35f47a392
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-12 16:07:26 +0200

    Added clickjack exempt headers

--- cicu/views.py
+++ cicu/views.py
@@ -1,7 +1,9 @@
+
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils import simplejson
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.views.decorators.clickjacking import xframe_options_exempt
 from django.core.files import File
 
 from PIL import Image
@@ -22,6 +24,7 @@ logger = logging.getLogger('django.request')
 
 
 @csrf_exempt
+@xframe_options_exempt
 @require_POST
 def upload(request):
     form = UploadedFileForm(data=request.POST, files=request.FILES)
@@ -47,6 +50,7 @@ def upload(request):
         return HttpResponseBadRequest(simplejson.dumps({'errors': form.errors}))
 
 @csrf_exempt
+@xframe_options_exempt
 @require_POST
 def crop(request):
     #try:
